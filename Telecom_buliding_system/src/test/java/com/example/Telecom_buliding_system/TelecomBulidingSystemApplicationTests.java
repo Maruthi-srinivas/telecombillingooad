775a7fd4d5aa5f529fa44eced16b0b50b7711bc0
Maruthi-srinivas <kavurumaruthisrinivas@gmail.com>
2025-04-08 16:52:20 +0530

    login page added to navbar

--- Telecom_buliding_system/src/test/java/com/example/Telecom_buliding_system/TelecomBulidingSystemApplicationTests.java
+++ Telecom_buliding_system/src/test/java/com/example/Telecom_buliding_system/TelecomBulidingSystemApplicationTests.java
@@ -6,8 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class TelecomBulidingSystemApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
